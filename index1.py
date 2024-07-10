15f5054a8a0d128dfa1b266967a4b34a44dffb45
JulianSaliqunaj <juliansaliqunaj8@gmail.com>
2024-07-10 11:50:08 +0200

    version 1

---
+++ index1.py
@@ -0,0 +1,13 @@
+x = int(input("shkruaje numrin e pikeve:"))
+if (x >= 0) and (x <- 29):
+    print("ti ke ngel edhe ki noten 1")
+elif(x >= 30) and (x <= 49):
+    print("ti e ki noten 2sh")
+elif (x >= 50) and ( x <= 69):
+    print("ti e ki noten 3sh")
+elif(x >= 70) and (x <= 89):
+    print("ti e ki noten 4sh")
+elif(x >= 90) and (x <= 100):
+    print("ti e ki noten sh")
+else:
+    print("mi ke jep piket me shum se 100"):
